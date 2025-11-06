bb79d842b69a4e42a98c56690433faeeddd2d57e
wazejinx <marwan.sazalinsyah@gmail.com>
2025-11-06 13:46:30 +0800

    bgbgbg

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,16 +4,19 @@
     alt="Background"
     class="absolute inset-0 w-full h-full object-cover" /> -->
 
-  <div class="custom-background-image h-20">Content with background here</div>
-
   <div class="min-h-screen flex flex-col bg-gray-50">
-    <main class="flex-1">
-      <slot />
-    </main>
+    <div class="custom-background-image flex-1">
+      <main class="flex-1">
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
 <style scoped>
 .custom-background-image {
-  background-image: url(/public/BG.svg);
+  background-image: url("~/assets/bg/Background-02.svg");
+  background-repeat: no-repeat;
+  background-size: cover; /* makes it fill the background */
+  background-position: center; /* centers the image */
 }
 </style>
