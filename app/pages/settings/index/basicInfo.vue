b03bbdb558f3b93a8c5239ccb3d58ba9a86dca83
ImnMuqri <imnmuqri@gmail.com>
2025-12-02 17:05:20 +0800

    Latest Ui for mobile responsive

--- app/pages/settings/index/basicInfo.vue
+++ app/pages/settings/index/basicInfo.vue
@@ -4,13 +4,21 @@
       <div class="flex items-center gap-6">
         <div class="p-8 rounded-full bg-white"></div>
         <div>
-          <p class="text-xl font-semibold">TrueProTech</p>
+          <div class="flex items-center gap-3">
+            <p class="text-xl font-semibold">TrueProTech</p>
+            <UiIcon
+              icon="cuida:edit-outline"
+              custom-class="w-4 h-4 text-[#00BDA7]"
+              class="block lg:hidden" />
+          </div>
           <p class="text-[12px] text-gray-400">trueprotech@gmail.com</p>
         </div>
       </div>
-      <UiButton variant="outline">Edit</UiButton>
+      <div>
+        <UiButton class="hidden lg:block" variant="outline">Edit</UiButton>
+      </div>
     </div>
-    <div class="grid grid-cols-1 gap-4 w-[500px]">
+    <div class="grid grid-cols-1 gap-4 w-full lg:w-[500px]">
       <UiInput label="Name" placeholder="Enter here.." dark></UiInput>
       <UiInput label="Email" placeholder="Enter here.." dark></UiInput>
       <UiInput label="Password" placeholder="Enter here.." dark></UiInput>
